5419e19a6356aaf181b58c2b8c5dbfd63384889c
JonatanSegal <82033910+JonatanSegal@users.noreply.github.com>
2022-04-24 22:33:18 +0200

    Forweek12 branch

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { renderTemplate, setActive, showPage } from "./utils.js"
-import { setupP2Handlers } from "./js-for-pages/page2.js"
-import { setUpPage3Handlers, getAllUsers } from "./js-for-pages/page3.js"
+
 
 
 
@@ -8,20 +7,17 @@ import { setUpPage3Handlers, getAllUsers } from "./js-for-pages/page3.js"
 function renderMenuItems(evt) {
   const element = evt.target
   setActive(element)
-  const id = element.id;
+  const id = element.id
   renderTemplate(id)  //This setups the HTML for the page
   switch (id) {
-    //Here you can execute JavaScript for the selected page
+      //Here you can execute JavaScript for the selected page
     case "page-1": {
       break
     }
     case "page-2": {
-      setupP2Handlers()
       break
     }
     case "page-3": {
-      setUpPage3Handlers()
-      getAllUsers()
       break
     }
   }
@@ -29,7 +25,3 @@ function renderMenuItems(evt) {
 
 document.getElementById("menu").onclick = renderMenuItems;
 showPage("page-1") //Set the default page to render
-
-
-
-
